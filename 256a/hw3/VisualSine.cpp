a9ee38543a8e0970e853c71dffa34797d92aaf68
mrotondo <mrotondo@gmail.com>
2010-10-26 14:44:30 -0700

    working version of hw3, still need to get on ccrma stations, hopefully include bonk + rhythm display + more pretties

--- 256a/hw3/VisualSine.cpp
+++ 256a/hw3/VisualSine.cpp
@@ -25,6 +25,8 @@ using namespace std;
 #include "chuck_fft.h"
 #include <pthread.h>
 
+#include "xtract_vector.h"
+
 // some defines
 #define SAMPLE float
 #define MY_PIE 3.14159265358979
@@ -32,18 +34,26 @@ using namespace std;
 
 pthread_mutex_t buf_mutex;
 
-
 // width and height of the window
 GLsizei g_width = 800;
 GLsizei g_height = 600;
 
 // globle buffer
 SAMPLE **g_freq_buffers;
-unsigned int g_num_freq_buffers = 10;
+unsigned int g_num_freq_buffers = 20;
 unsigned int g_freq_buffer_front_index = 0;
 long g_freq_buffer_size; // size of buffer in samples
 
 float *g_window;
+
+float g_interesting_freqs_sum = 0;
+float g_prev_interesting_freqs_sum = 0;
+
+float g_average_bin = 0;
+
+GLfloat z_rot = 0.0;
+GLfloat y_rot = 0.0;
+GLfloat x_rot = 0.0;
 
 // audio callback
 int callme( char * buffer, int buffer_size, void * user_data )
@@ -51,9 +61,8 @@ int callme( char * buffer, int buffer_size, void * user_data )
   //cout << "Receiving buffer of size " << buffer_size << endl;
     
   SAMPLE *buffy = (SAMPLE *)buffer;
-  SAMPLE *freq_buf = g_freq_buffers[g_freq_buffer_front_index];
-
   pthread_mutex_lock(&buf_mutex);    
+  SAMPLE *freq_buf = g_freq_buffers[g_freq_buffer_front_index];
   for( int i = 0; i < buffer_size; i += 2 ) {
     // copy to global buffer
     freq_buf[i/2] = buffy[i];
@@ -63,6 +72,22 @@ int callme( char * buffer, int buffer_size, void * user_data )
   //cout << "Windowing " << g_freq_buffer_size << " samples in g_buffer" << endl;
   apply_window(freq_buf, g_window, g_freq_buffer_size);
   rfft(freq_buf, g_freq_buffer_size / 2, 1);
+  g_freq_buffer_front_index++;
+  g_freq_buffer_front_index %= g_num_freq_buffers;
+
+  float bin_total = 0;
+
+  float new_interesting_freqs_sum = 0;
+  for (int i = 0; i < g_freq_buffer_size / 2; i++) {
+    float abs_val = cmp_abs(((complex *)freq_buf)[i]);
+    new_interesting_freqs_sum += abs_val;
+    bin_total += i * abs_val;
+  }
+  g_interesting_freqs_sum = (g_prev_interesting_freqs_sum + new_interesting_freqs_sum) / 2;
+  g_prev_interesting_freqs_sum = new_interesting_freqs_sum;
+
+  g_average_bin = bin_total / new_interesting_freqs_sum;
+
   pthread_mutex_unlock(&buf_mutex);
   
   return 0;
@@ -106,10 +131,26 @@ int main( int argc, char ** argv )
   // set the mouse function - called on mouse stuff
   glutMouseFunc( mouseFunc );
   
+  GLfloat fogColor[4]= {1.0f, 1.0f, 1.0f, 1.0f};		// Fog Color
+  glClearColor(1.0f,1.0f,1.0f,1.0f);	  // We'll Clear To The Color Of The Fog ( Modified )
+  
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glFogi(GL_FOG_MODE, GL_LINEAR);		// Fog Mode
+  glFogfv(GL_FOG_COLOR, fogColor);			// Set Fog Color
+  glFogf(GL_FOG_DENSITY, 0.7f);				// How Dense Will The Fog Be
+  glHint(GL_FOG_HINT, GL_NICEST);			// Fog Hint Value
+  glFogf(GL_FOG_START, 0.0f);				// Fog Start Depth
+  glFogf(GL_FOG_END, 20.0f);				// Fog End Depth
+  glEnable(GL_FOG);					// Enables GL_FOG
+  
+  glEnable(GL_LINE_SMOOTH);
+
   // RtAudio pointer
   RtAudio * audio = NULL;
   // buffer size
-  int buffer_size = 2048;
+  int buffer_size = 1024;
   
   // create the RtAudio
   try {
@@ -200,15 +241,18 @@ void reshapeFunc( GLsizei w, GLsizei h )
 //-----------------------------------------------------------------------------
 void keyboardFunc( unsigned char key, int x, int y )
 {
-    switch( key )
+  switch( key )
     {
-        case 'Q':
-        case 'q':
-            exit(1);
-            break;
+    case 'r':
+      //cout << "HELLO R" << endl;
+      break;
+    case 'Q':
+    case 'q':
+      exit(1);
+    break;
     }
-    
-    glutPostRedisplay( );
+  
+  glutPostRedisplay( );
 }
 
 
@@ -270,54 +314,111 @@ void idleFunc( )
 void displayFunc( )
 {
     // local state
-    static GLfloat zrot = 0.0f, c = 0.0f;
+    static GLfloat c = 0.0f;
 
     // clear the color and depth buffers
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    
-    // line width
-    glLineWidth( 1.0 );
-    // step through and plot the waveform
-    GLfloat x = -5;
-    // increment
-    GLfloat xinc = ::fabs(2*x / (g_freq_buffer_size / 2));
-    
-    // push the matrix
-    glPushMatrix();
 
-    // rotation
-    //glRotatef( zrot, 0, 0, 1 );
-    zrot += .1;
+    glPushMatrix(); // Go to back of waterfall
+    glTranslatef(0, 0, -(GLfloat)g_num_freq_buffers * 0.5);
 
-    glTranslatef(0.0, 0.0, -10.0);
-
-    // go
-    glBegin( GL_LINE_STRIP );
-    // loop through global buffer
     pthread_mutex_lock(&buf_mutex);
-    float prev_y_val = 0;
-    SAMPLE *freq_buf = g_freq_buffers[g_freq_buffer_front_index];
-    for( int i = 0; i < g_freq_buffer_size / 2; i++ )
-    {
-        // set the color
-        glColor3f( (sin(c)+1)/2, (sin(c*2)+1)/2, (sin(c+.5)+1)/2 );
-        // set the next vertex
-	float y_val = -10 * pow(fabs(log10(fabs(freq_buf[i]) + 0.01)), .5) + 13;
-        glVertex2f( x, (y_val + prev_y_val) / 2);
-	prev_y_val = y_val;
-	//cout << "here's a value: " << y_val << endl;
-        // increment x
-        x += xinc;
+    GLfloat x;
+    for (int buf_idx = 0; buf_idx < g_num_freq_buffers; buf_idx++) {
+      // push the matrix
+      glPushMatrix();
+      // line width
+      //glLineWidth( 5 * buf_idx / g_num_freq_buffers );
+
+      glTranslatef(0.0, 0.0, (GLfloat)buf_idx * 0.5);
+      x = -5;
+      GLfloat xinc = ::fabs(2*x / (g_freq_buffer_size / 4));
+      
+      // go
+      glBegin( GL_LINE_STRIP );
+      // loop through global buffer
+      float prev_y_val = 0;
+
+      complex *freq_buf = (complex *)g_freq_buffers[(g_freq_buffer_front_index + buf_idx) 
+						    % g_num_freq_buffers];
+      
+      //SAMPLE *freq_buf = g_freq_buffers[(g_freq_buffer_front_index + buf_idx + 1) 
+      //% g_num_freq_buffers];
+
+      if (buf_idx == g_num_freq_buffers - 1) {
+	glLineWidth(2.0);
+	glColor3f(0.0, 0.4, 0.0);
+      } else {
+	glLineWidth(1.0);
+	glColor4f( 0.8 * (0.2 * (sin(c+.8)+1)/2), 0.8 + (0.2 * (sin(c+.01)+1)/2), 0.7 + 0.3 * ((sin(c+.5)+1)/2), 0.5);
+      }
+
+      int freq_idx;
+      for( int i = 0; i < g_freq_buffer_size / 4; i++ )
+	{
+	  // set the next vertex
+
+	  freq_idx = i;
+	  if (i > 0) {
+	    freq_idx = log(i) * i;
+	  }
+
+	  //float y_val = log10(cmp_abs(freq_buf[i])) + 3;
+	  float y_val = 20 * pow(cmp_abs(freq_buf[i]), 0.5) - 1;
+
+	  glVertex2f( x, 1.2 * (y_val + prev_y_val) / 2);
+	  prev_y_val = y_val;
+	  //cout << "here's a value: " << y_val << endl;
+
+	  // increment x
+	  x += xinc;
+	}
+      // done
+      glEnd();
+      
+      // pop
+      glPopMatrix();
     }
-    pthread_mutex_unlock(&buf_mutex);
-    // done
-    glEnd();
+
+    //cout << "Interesting freq mag sum: " << g_interesting_freqs_sum << endl;
     
     // increment color
     c += .001;
     
-    // pop
     glPopMatrix();
+    pthread_mutex_unlock(&buf_mutex);
+    
+
+    glPushMatrix(); // power sphere
+    
+    glTranslatef(0.0, 0.0, 0.0);
+    glColor4f(0.0 + g_interesting_freqs_sum, 
+	      0.5 + g_interesting_freqs_sum / 2,
+	      1.0 - g_interesting_freqs_sum,
+	      0.3);
+    
+    GLfloat radius = pow(g_interesting_freqs_sum + 1, 2);
+    glutSolidSphere(radius, 16, 16);
+    
+    glPopMatrix(); // power sphere
+
+    glPushMatrix(); // freq sphere
+
+    x_rot += 0.47;
+    y_rot += 0.03;
+    z_rot += 0.11;
+    glRotatef(x_rot, 1.0, 0.0, 0.0);
+    glRotatef(y_rot, 0.0, 1.0, 0.0);
+    glRotatef(z_rot, 0.0, 0.0, 1.0);
+    glTranslatef(radius * 1.2, 0.0, 0.0);
+
+    glColor4f(20 * g_average_bin / g_freq_buffer_size,
+	      0.4,
+	      0.1,
+	      0.6);
+    glutSolidSphere(0.1, 8, 8);
+
+    glPopMatrix(); // freq sxphere
 
     // flush!
     glFlush( );
