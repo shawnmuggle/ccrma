55839e40eceb2603a5c951fc876e9add7ed65603
mrotondo <mrotondo@gmail.com>
2011-04-11 01:29:13 -0700

    i doneveno

--- 424/golay.m
+++ 424/golay.m
@@ -1,11 +1,13 @@
-function [a b g] = golay (n)
+function [responses] = golay ()
 a = [1];
 b = [1];
-while n > 0
+n = 10;
+while n >= 0
     n = n - 1;
     aNext = [a b];
     bNext = [a b*-1];
     a = aNext;
     b = bNext;
+    
 end
 g = xcorr(a, a) + xcorr(b, b);
