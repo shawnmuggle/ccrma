e6957f5cd6359d622567283be570a8b97cbc06fb
Mike Rotondo <mrotondo@gmail.com>
2011-10-11 15:54:41 -0700

    first commit of assignment 2 boilerplate

---
+++ 148/assignment2/assignment2/main.cpp
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "sgl.h"
+#include "st.h"
+
+using namespace std;
+
+STImage* buff;
+
+void display( void )
+{
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	// --- Make drawing calls here ---+
+
+
+
+	// --- End of drawing calls ------+
+
+	buff->Draw();
+	glutSwapBuffers();
+}
+
+void reshape( int w, int h )
+{
+  glMatrixMode( GL_PROJECTION );
+  glLoadIdentity();
+
+  glOrtho( 0., w, 0., h, -1., 1. );
+  glViewport( 0, 0, w, h );
+  setBufferSize(w, h);
+
+  glutPostRedisplay();
+}
+
+void keyboard( unsigned char key, int x, int y )
+{
+	switch(key)
+	{
+	case 27: // Escape key
+		exit(0);
+		break;
+	case 's': // Save
+		buff->Save("output.png");
+		break;
+	}
+}
+
+int main (int argc, char *argv[])
+{
+	int win_width = 512;
+	int win_height = 512;
+	buff = new STImage(win_width, win_height, STColor4ub(0, 0, 0, 255));
+	setBuffer(buff);
+
+	glutInit( &argc, argv );
+	glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
+	glutInitWindowSize( win_width, win_height );
+
+	glutCreateWindow( "Intro Graphics Assignment 2" );
+
+	glutDisplayFunc( display );
+	glutReshapeFunc( reshape );
+	glutKeyboardFunc( keyboard );
+
+	glutMainLoop();
+
+	delete buff;
+}
---
+++ 148/assignment2/libsgl/sgl.cpp
@@ -0,0 +1,66 @@
+#include "sgl.h"
+
+using namespace std;
+// --- Do not modify this code ---+
+#define IMPLEMENT_THIS_FUNCTION printf("Warning: call to unimplimented function!\n")
+STImage* img;
+int buffer_width;
+int buffer_height;
+void setBuffer(STImage* ptr) { img = ptr; }
+void setBufferSize(int w, int h) { buffer_width = w; buffer_height = h; }
+// --- End of do not modify this code ---+
+
+void renderTriangles()
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglBeginTriangles()
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglEnd()
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglLoadIdentity()
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglScale(SGLfloat xscale, SGLfloat yscale)
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglTranslate(SGLfloat xtrans, SGLfloat ytrans)
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglRotate(SGLfloat angle)
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglPushMatrix()
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglPopMatrix()
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglVertex(SGLfloat x, SGLfloat y)
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
+
+void sglColor(SGLfloat r, SGLfloat g, SGLfloat b)
+{
+	IMPLEMENT_THIS_FUNCTION;
+}
